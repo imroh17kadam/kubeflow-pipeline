7098a5199de925917102d0ce48f623672bcdcc53
imroh17kadam <kadam17rohit@gmail.com>
2025-12-24 21:24:47 +0530

    R2 Score Added

--- main.py
+++ main.py
@@ -6,6 +6,6 @@ if __name__ == "__main__":
     df = load_data("data/raw/boston.csv")
     X_train, X_test, y_train, y_test = preprocess_data(df, target_column="medv")
     model = train_model(X_train, y_train)
-    mse = evaluate_model(model, X_test, y_test)
+    mse, r2 = evaluate_model(model, X_test, y_test)
     save_model(model)
-    print(f"Model trained and saved! Test MSE: {mse}")
+    print(f"Model trained and saved! Test MSE: {mse}, Test R2: {r2}")
